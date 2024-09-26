32b391131b46d8174075827d5b4adbd5cd4156ef
Aibek <dokaidea@g,ail.com>
2024-09-26 19:44:52 +0600

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './App.css'
-import {useState} from "react";
-import Toggler from "./Toggler.tsx";
+import Webcam from "react-webcam";
+import {useRef} from "react";
 
 function App() {
-    const [isToggled, setIsToggled] = useState<boolean>(false);
-
-    const toggle = () => {
-        setIsToggled(!isToggled);
-    };
+    const webcamRef = useRef<Webcam | null>(null);
 
   return (
       <>
-          <div>
-              <button onClick={toggle}>
-                  {isToggled ? 'Выключено' : 'Включено'}
-              </button>
-              <p>Состояние: {isToggled ? 'Включено' : 'Выключено'}</p>
-          </div>
-          <Toggler/>
+        <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={{facingMode: "environment"}}
+        />
       </>
   )
 }
